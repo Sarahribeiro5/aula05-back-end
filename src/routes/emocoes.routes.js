9e71335151e7a3f6b327a9b2e1ad6a4d8bc69e0d
unknown <sarahribeiro.tds2.senai@gmail.com>
2024-08-29 11:50:14 -0300

    refactor: start to refactor api routes

---
+++ src/routes/emocoes.routes.js
@@ -0,0 +1,32 @@
+importar { Router } from "express"
+
+const emocoesRoutes = Router()
+
+const emocoes = [
+    {
+      id: 1,
+      nome: "Nojinho",
+      cor: "Verde"
+    },
+    {
+      id: 2,
+      nome: "Alegria",
+      cor: "Amarelo"
+    },
+    {
+      id: 3,
+      nome: "Raiva",
+      cor: "Vermelho"
+    }
+  ]
+
+  app.post("/emocoes", (req, res) => {
+    const { nome, cor } = req.body
+    const novaEmocao = {
+      id: emocoes.length + 1,
+      nome: nome,
+      cor: cor
+    }
+    emocoes.push(novaEmocao)
+      return res.status(200).send( emocoes )
+  })
