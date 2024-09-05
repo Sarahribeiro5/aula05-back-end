4d72634996ef002384d24985b66eead6b70d0c31
unknown <sarahribeiro.tds2.senai@gmail.com>
2024-09-05 17:20:46 -0300

    feat: create get by id method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -20,20 +20,41 @@ let emocoes = [
   }
 ]
 
+//rota para buscar todas as emoções
 emocoesRoutes.get("/",(req, res ) => {
   return res.status(200).send(emocoes)
 })
 
+//Rota para criar uma nova emoção
 emocoesRoutes.post("/", (req, res) => {
   const {nome, cor } = req.body
   const novaEmocao = {
-    id: emocoes.lenght + 1,
+    id: emocoes.length + 1,
     nome: nome,
     cor: cor
   }
   emocoes.push(novaEmocao)
   return res.status(201).send ( emocoes )
-})
+});
+
+//Rota para buscar uma emoção pelo id
+emocoesRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  //console.log(id);
+  const emocao = emocoes.find((emotion) => emotion.id == id )
+
+  if (!emocao) {
+    return res.status(404).send({
+      message: "Emoção não encontrada!",
+    });
+  }
+
+  return res.status(200).send({
+    message: "Emoção encontrada",
+    emocao,
+  });
+});
 
  
 export default emocoesRoutes
