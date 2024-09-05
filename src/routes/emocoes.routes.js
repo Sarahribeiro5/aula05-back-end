1b809a068a0ff8d3832c680aea4b9fab8576ed48
unknown <sarahribeiro.tds2.senai@gmail.com>
2024-09-05 15:42:58 -0300

    feat: create routes configs

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -1,32 +1,39 @@
-importar { Router } from "express"
+import { Router } from "express"
 
 const emocoesRoutes = Router()
 
-const emocoes = [
-    {
-      id: 1,
-      nome: "Nojinho",
-      cor: "Verde"
-    },
-    {
-      id: 2,
-      nome: "Alegria",
-      cor: "Amarelo"
-    },
-    {
-      id: 3,
-      nome: "Raiva",
-      cor: "Vermelho"
-    }
-  ]
+let emocoes = [
+  {
+    id: 1,
+    nome: "Nojinho",
+    cor: "Verde"
+  },
+  {
+    id: 2,
+    nome: "Alegria",
+    cor: "Amarelo"
+  },
+  {
+    id: 3,
+    nome: "Raiva",
+    cor: "Vermelho"
+  }
+]
 
-  app.post("/emocoes", (req, res) => {
-    const { nome, cor } = req.body
-    const novaEmocao = {
-      id: emocoes.length + 1,
-      nome: nome,
-      cor: cor
-    }
-    emocoes.push(novaEmocao)
-      return res.status(200).send( emocoes )
-  })
+emocoesRoutes.get("/",(req, res ) => {
+  return res.status(200).send(emocoes)
+})
+
+emocoesRoutes.post("/", (req, res) => {
+  const {nome, cor } = req.body
+  const novaEmocao = {
+    id: emocoes.lenght + 1,
+    nome: nome,
+    cor: cor
+  }
+  emocoes.push(novaEmocao)
+  return res.status(201).send ( emocoes )
+})
+
+ 
+export default emocoesRoutes
--- src/server.js
+++ src/server.js
@@ -1,30 +1,16 @@
 import express from "express";
 import { config } from "dotenv";
 
+import routes from "./routes/index.routes.js";
+
 config();
 
 const serverPort = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
-const emocoes = [
-  {
-    id: 1,
-    nome: "Nojinho",
-    cor: "Verde"
-  },
-  {
-    id: 2,
-    nome: "Alegria",
-    cor: "Amarelo"
-  },
-  {
-    id: 3,
-    nome: "Raiva",
-    cor: "Vermelho"
-  }
-]
 
 const personagens = [
   {
@@ -46,31 +32,6 @@ const personagens = [
     vivo: false
   }
 ]
-
-app.get("/", (req, res) => {
-    return res.status(200).send({ message: "Hello, World!" })
-})
-
-app.get("/2tds2", (req, res) => {
-    return res.status(200).send({ message: "Hello, World!" })
-})
-app.get("/emocoes", (req, res) => {
-    return res.status(200).send( emocoes )
-})
-app.get("/personagens", (req, res) => {
-    return res.status(200).send( personagens )
-})
- 
-app.post("/emocoes", (req, res) => {
-  const { nome, cor } = req.body
-  const novaEmocao = {
-    id: emocoes.length + 1,
-    nome: nome,
-    cor: cor
-  }
-  emocoes.push(novaEmocao)
-    return res.status(200).send( emocoes )
-})
 
 app.listen(serverPort, () => {
   console.log(`⚡ Server started on http://localhost:${serverPort}`);
