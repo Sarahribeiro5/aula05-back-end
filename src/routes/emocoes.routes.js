ba8305b4ab0e802d39c9b79ea8eb54676f2698a4
unknown <sarahribeiro.tds2.senai@gmail.com>
2024-09-26 15:15:07 -0300

    feat: create delete emotion method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -57,4 +57,47 @@ emocoesRoutes.get("/:id", (req, res) => {
 });
 
  
+emocoesRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const emocao = emocoes.find((emotion) => emotion.id == id )
+
+  if (!emocao) {
+    return res.status(404).send({
+      message: "Emoção não encontrada!",
+    });
+  }
+
+
+const {nome, cor } = req.body;
+emocao.nome = nome;
+emocao.cor = cor;
+
+return res.status(200).send({
+  message: "Emoção atualizada",
+  emocao,
+});
+});
+
+emocoesRoutes.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const emocao = emocoes.find((emotion) => emotion.id == id )
+
+  if (!emocao) {
+    return res.status(404).send({
+      message: "Emoção não encontrada!",
+    });
+  }
+
+  emocoes = emocoes.filter((emotion) => emotion.id != id );
+
+
+  return res.status(200).send({
+    message: "Emoção deletada!",
+    emocao,
+  });
+});
+
+
 export default emocoesRoutes
